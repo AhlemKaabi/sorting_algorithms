ad674cad9cd2535e3b7d8db7c22b027f4d7a8ac7
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-06-16 18:14:41 +0100

    shell sort algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,32 +9,32 @@
 */
 void shell_sort(int *array, size_t size)
 {
-    size_t  i, j;
-    size_t gap = 1;
-    int value_insert;
+	size_t  i, j;
+	size_t gap = 1;
+	int value_insert;
 
-    if (array == NULL || size < 2)
-    {
-        return;
-    }
-    while (gap < size / 3)
-    {
-        gap  = gap * 3 + 1;
-    }
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            value_insert = array[i];
-            j = i;
-            while (j > gap - 1 && array[j - gap] > value_insert)
-            {
-                array[j] = array[j - gap];
-                j = j - gap;
-            }
-            array[j] = value_insert;
-        }
-    gap = (gap - 1) / 3;
-    print_array(array, size);
-    }
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	while (gap < size / 3)
+	{
+		gap  = gap * 3 + 1;
+	}
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			value_insert = array[i];
+			j = i;
+			while (j > gap - 1 && array[j - gap] > value_insert)
+			{
+				array[j] = array[j - gap];
+				j = j - gap;
+			}
+			array[j] = value_insert;
+		}
+	gap = (gap - 1) / 3;
+	print_array(array, size);
+	}
 }
